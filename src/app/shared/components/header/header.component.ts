71f75ba0d842999d97c522c1fe8f05c21422090e
kevip <castaneda.kevin114@gmail.com>
2024-04-07 22:17:14 -0500

    feat(styles): Add mobile/desktop header styles

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,8 +2,10 @@ import { Component, inject } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { SideNavService } from '../../services/side-nav.service';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+
+import { SideNavService } from '../../services/side-nav.service';
 
 @Component({
   selector: 'eznance-header',
@@ -13,6 +15,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatMenuModule,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
@@ -20,4 +23,9 @@ import { MatButtonModule } from '@angular/material/button';
 export class HeaderComponent {
   public sideNavService = inject(SideNavService);
 
+  public menuItems = [
+    { name: 'Home' },
+    { name: 'Profile' },
+    { name: 'Statistics' }
+  ]
 }
